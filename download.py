d1077b2f5b9ba3f18db9687b23c00944bbad9f52
Michael Anderson <42286834+Mandersoon@users.noreply.github.com>
2018-12-30 21:42:31 -0800

    Added re.search capabilities as first step to data santization

--- download.py
+++ download.py
@@ -1,11 +1,13 @@
 import os
 from pathlib import Path
 import requests
+import re
 
 # Importing data from the ShipList txt file.
 with open("ShipList.txt", "r") as ShipList:
     ships = ShipList.read().splitlines()
 
+# Set the directory to place ShipFile txt files.
 working_dir = os.getcwd()
 ship_files_dir = os.path.join(working_dir, "ShipFiles")
 print("Setting download path to " + ship_files_dir + ".")
@@ -13,16 +15,26 @@ if not os.path.exists(ship_files_dir):
     os.mkdir(ship_files_dir)
 
 for ship in ships:
-    r = requests.get("https://azurlane.koumakan.jp/w/index.php?title=" + ship + "&action=edit")
-    ship_file = ship + ".txt"
     print("Downloading file for " + ship + "...")
+    # Use get request to download from the wiki
+    try:
+        page = requests.get("https://azurlane.koumakan.jp/w/index.php?title=" + ship + "&action=edit")
+    except:
+        print("Unable to access the website - please make sure you're able to access the wiki and/or the internet.")
+        break
+    print("Santitizing data...")
+    
+    # Magical regex to extract only the relevant data from the ShipMain class rather than all of the other unecessary HTML data.
+    filtered_data = re.search('{{ShipMain(.|\n)*}}\n', page.text)
+    
+    # re.search returns data in groups, group 0 has the info we need. If an exception occurs, that means it can't find {{ShipMain}}, and is therefore not a valid ship page.
+    try:
+        result = filtered_data.group(0)
+    except AttributeError:
+        print("Unable to find the proper ship data class for " + ship + ". Verify the spelling is correct and that the page itself exists.")
+        continue
+
+    ship_file = ship + ".txt"
     outfile = os.path.join(ship_files_dir, ship_file)
-    with open(outfile, 'wb') as f:
-        lines = r.content.splitlines()
-        for line in lines:
-            f.write(line)
-            f.write(b'\n')
-    fileinfo = os.stat(outfile)
-    filesize = int(fileinfo.st_size)
-    if not 30000 < filesize < 75000:
-        print("The file size for " + ship + " is abnormal, please confirm correct spelling/punctuation. See readme for details.")
+    with open(outfile, 'a', encoding='utf-8') as shipdata:
+        shipdata.write(result)
