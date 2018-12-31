3f1c23a2249c156a12d96b88eaeb0b45eaad0962
=Mandersoon <=42286834+Mandersoon@users.noreply.github.com>
2018-12-30 22:08:09 -0800

    Hopefully this fixes it...

--- download.py
+++ download.py
@@ -38,16 +38,3 @@ for ship in ships:
     outfile = os.path.join(ship_files_dir, ship_file)
     with open(outfile, 'a', encoding='utf-8') as shipdata:
         shipdata.write(result)
-    r = requests.get("https://azurlane.koumakan.jp/w/index.php?title=" + ship + "&action=edit")
-    ship_file = ship + ".txt"
-    print("Downloading file for " + ship + "...")
-    outfile = os.path.join(ship_files_dir, ship_file)
-    with open(outfile, 'wb') as f:
-        lines = r.content.splitlines()
-        for line in lines:
-            f.write(line)
-            f.write(b'\n')
-    fileinfo = os.stat(outfile)
-    filesize = int(fileinfo.st_size)
-    if not 30000 < filesize < 75000:
-        print("The file size for " + ship + " is abnormal, please confirm correct spelling/punctuation. See readme for details.")
