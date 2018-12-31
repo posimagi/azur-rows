7800caddb73cc09e7ec2c53ee02051eabb0c273e
Mandersoon <nope@nope.com>
2018-12-30 19:30:30 -0800

    Added try/catch, linked stat list to .txt, added CSV functionality

--- collate.py
+++ collate.py
@@ -2,14 +2,9 @@ import os
 from pathlib import Path
 import csv
 
-# === CHANGE ME: Add additional stats here, ===
-#     in the form of the MediaWiki variables
-#     found on the edit pages for each ship
-#     (e.g. https://azurlane.koumakan.jp/w/index.php?title=Hood&action=edit)
-stats = ["HealthMax",
-         "AAMax",
-         "Nationality",
-         "Type"]
+with open("StatList.txt", "r") as StatList:
+    stats = StatList.read().splitlines()
+
 # === DO NOT EDIT BELOW THIS LINE ===
 
 types = ["Light Cruiser",
@@ -33,7 +28,12 @@ paths = [os.path.join(ship_files_dir, f) for f in os.listdir(ship_files_dir)
 header = "Name"
 for stat in stats:
     header += "," + str(stat)
-print(header)
+
+print("Generating CSV at " + working_dir + ".")
+
+with open("Compilation.csv", 'w', newline='') as ship_csv:
+    ship_csv.write(header)
+    ship_csv.write('\n')
 
 # Generate a row for each file
 for name, path in zip(names, paths):
@@ -42,11 +42,19 @@ for name, path in zip(names, paths):
         for line in body:
             for index, stat in enumerate(stats):
                 if stat in str(line):
-                    value = line.decode('utf-8').split('=')[1].strip()
+                    try:
+                        value = line.decode('utf-8').split('=')[1].strip()
+                    except IndexError:
+                        print("There was an invalid " + stat + " found on " + name + "'s page. Feel free to ignore, or see the readme.")
+                    except KeyError:
+                        print("We can't find " + stat + "in the page for " + name + ". Please see the readme.")
                     if stat == "Type" and value not in types:
                         continue
                     values[stat] = value
     result = name
-    for stat in stats:
-        result += ',' + values[stat]
-    print(result)
+    with open("Compilation.csv", 'a', newline='') as ship_csv:
+        for stat in stats:
+            result += ',' + values[stat]
+        ship_csv.write(result)
+        ship_csv.write('\n')
+        print("Row added for " + name + ".")
