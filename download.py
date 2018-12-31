5aea410b929e0894a0457590e4023a6b0c619d75
Mandersoon <nope@nope.com>
2018-12-30 19:31:38 -0800

    Ship list now external .txt, clearer status messages, file size warning

--- download.py
+++ download.py
@@ -2,9 +2,9 @@ import os
 from pathlib import Path
 import requests
 
-# === CHANGE ME: Add ship names here to scrap more info ===
-ships = ["San Diego", "Hood"]
-# === DO NOT EDIT BELOW THIS LINE ===
+# Importing data from the ShipList txt file.
+with open("ShipList.txt", "r") as ShipList:
+    ships = ShipList.read().splitlines()
 
 working_dir = os.getcwd()
 ship_files_dir = os.path.join(working_dir, "ShipFiles")
@@ -18,17 +18,21 @@ for ship in ships:
 pages = []
 
 for ship in ship_s:
-    print("Downloading file for " + ship + "...")
+    print(ship + " added to the list!")
     pages.append(
         "https://azurlane.koumakan.jp/w/index.php?title=" + ship + "&action=edit")
 
 for ship, page in zip(ship_s, pages):
     r = requests.get(page)
     ship_file = ship + ".txt"
-    print("Writing file for " + ship + "...")
+    print("Downloading file for " + ship + "...")
     outfile = os.path.join(ship_files_dir, ship_file)
     with open(outfile, 'wb') as f:
         lines = r.content.splitlines()
         for line in lines:
             f.write(line)
             f.write(b'\n')
+    fileinfo = os.stat(outfile)
+    filesize = int(fileinfo.st_size)
+    if not 30000 < filesize < 75000:
+        print("The file size for " + ship + " is abnormal, please confirm correct spelling/punctuation. See readme for details.")
