d247e635d200d317101c1dc72b140bf07de8e211
Mandersoon <42286834+Mandersoon@users.noreply.github.com>
2019-01-03 11:18:56 -0800

    typos/formatting

--- download.py
+++ download.py
@@ -4,6 +4,7 @@ import re
 try:
     import requests
 except ModuleNotFoundError:
+    # Requests library is not installed in python by default
     print("You will need to install the requests library to continue. You may install it using Pip.")
 
 
@@ -22,20 +23,23 @@ for ship in ships:
     # Use get request to download from the wiki
     print("Downloading file for " + ship + "...")
     try:
-        page = requests.get("https://azurlane.koumakan.jp/w/index.php?title=" + ship + "&action=edit")
+        page = requests.get(
+            "https://azurlane.koumakan.jp/w/index.php?title=" + ship + "&action=edit")
     except:
         print("Unable to access the website - please make sure you're able to access the wiki and/or the internet.")
         break
-    print("Santitizing data...")
-    
+    print("Sanitizing data...")
+
     # Magical regex to extract only the relevant data from the ShipMain class rather than all of the other unecessary HTML data.
     filtered_data = re.search('{{ShipMain(.|\n)*}}\n', page.text)
-    
-    # re.search returns data in groups, group 0 has the info we need. If an exception occurs, that means it can't find {{ShipMain}}, and is therefore not a valid ship page.
+
+    # re.search returns data in groups, group 0 has the info we need.
+    # If an exception occurs, that means it can't find {{ShipMain}}, and is therefore not a valid ship page.
     try:
         result = filtered_data.group(0)
     except AttributeError:
-        print("Unable to find the proper ship data class for " + ship + ". Verify the spelling is correct and that the page itself exists.")
+        print("Unable to find the proper ship data class for " + ship +
+              ". Verify the spelling is correct and that the page itself exists.")
         continue
 
     # Stripping leading whitespace and pipes
